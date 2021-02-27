a07e0637438dd1b685930a56ba5012df1d2847a2
Miq Alarcia <miq145@gmail.com>
2021-02-27 10:18:46 +0100

    12 chistes en español

--- js/codi.js
+++ js/codi.js
@@ -1,22 +1,27 @@
 window.onload = function () {
-  tempsAvui(6167865);
+  tempsAvui();
 }
 
-const url = 'https://icanhazdadjoke.com';
+const url = '/js/acudits.json'; //'https://icanhazdadjoke.com';
 const nouXist = document.querySelector('#nuevoChiste');
 
+//const randomNum = Math.floor(Math.random() * (arrayAcudits[0].length - 1)) + 0;
+
 function sigChiste() {
+
+
   fetch(url, {
     // Sol.licitud d'API, capçelera
     method: "GET",
     headers: {
-      Accept: "application/json",
+      Accept: "application/json"
     },
   })
     .then(response => response.json())
     .then(response => {
       console.log(response);
-      nouXist.textContent = response.joke;
+      let randomIndex = Math.floor(Math.random() * response.arrayAcudits.length);
+      nouXist.textContent = response.arrayAcudits[randomIndex].acu;
       // nouXist.innerHTML = `${response.joke}
     }
     )
@@ -28,7 +33,6 @@ const temps = 'https://api.openweathermap.org/data/2.5/weather?q=' + ciutat + '&
 const ubcacio = document.querySelector('#ubicacio');
 const tempera = document.querySelector('#temperatura');
 const condicions = document.querySelector('#condis');
-// const iconaTemps = document.querySelector('#icona');
 
 function tempsAvui() {
   fetch(temps, {
@@ -44,6 +48,6 @@ function tempsAvui() {
       ubicacio.textContent = data.name;
       tempera.textContent = data.main.temp + "ºC";
       // iconaTemps.textContent = data.weather.icon;
-      // condicions.textContent = data.weather[0];
+      condicions.textContent = data.weather[0].description;
     })
 }
